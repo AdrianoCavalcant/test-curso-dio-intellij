1d26e7f5650f0e509fec227060d019709588f25c
AdrianoCavalcant <silvaadriano233@gmail.com>
2023-02-19 19:15:59 -0300

    first commit

---
+++ src/br/dio/com/model/Gato.java
@@ -0,0 +1,56 @@
+package br.dio.com.model;
+import java.util.Objects;
+
+public class Gato {
+
+    private String nome;
+    private String cor;
+    private String idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getcor() {
+        return cor;
+    }
+
+    public void setCor(String cep) {
+        this.cor = cor;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String nomeDoMeio) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato main = (Gato) o;
+        return Objects.equals(nome, main.nome) && Objects.equals(cor, main.cor) && Objects.equals(idade, main.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade='" + idade + '\'' +
+                '}';
+    }
+}
---
+++ src/br/dio/com/model/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.dio.com.model;
+
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+
+        System.out.println(gato);
+    }
+
+
+}
